afefd0ea6c5391f2edf602b012854ba5746379d6
eliealjalbout <elie.aljalbout@tum.de>
2020-06-05 12:22:32 +0200

    removed traces - stable version

--- sai2_environment/utils.py
+++ sai2_environment/utils.py
@@ -11,7 +11,6 @@ def name_to_task_class(task_file: str):
     name = task_file.replace('.py', '')
     class_name = ''.join([w[0].upper() + w[1:] for w in name.split('_')])
     try:
-        set_trace()
         mod = importlib.import_module("sai2_environment.tasks.%s" % name)
         mod = importlib.reload(mod)
     except ModuleNotFoundError as e:
