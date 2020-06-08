fcacd97a81d0fa7deec1793ca089f07e3cf42b8c
Max Ulmer <max.ulmer@tum.de>
2020-06-08 20:54:44 +0200

    minor stuff

--- real_robot_dEE_impedance.py
+++ real_robot_dEE_impedance.py
@@ -15,7 +15,7 @@ def main():
                    action_space=action_space,
                    isotropic_gains=True,
                    render=True,
-                   blocking_action=False,
+                   blocking_action=True,
                    rotation_axis=(0, 0, 1))    
 
     episodes = 50
@@ -29,7 +29,7 @@ def main():
         obs, reward, done, info = env.reset()
         for step in range(steps):
             position = np.around(np.random.uniform(low=-0.15, high=0.15, size=(3,)), 2)
-            rotation = np.around(np.random.uniform(low=0, high=0, size=(1,)), 2)
+            rotation = np.around(np.random.uniform(low=0.1, high=0.1, size=(1,)), 2)
             stiffness_linear = np.around(np.random.uniform(low=-50, high=50, size=(1,)),2)
             stiffness_rot = np.around(np.random.uniform(low=-2, high=2, size=(1,)), 2)
             action = np.concatenate((position,rotation,stiffness_linear,stiffness_rot))
