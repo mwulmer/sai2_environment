33b475eeace060aeba0158e85302e35056cfb70d
Max Ulmer <max.ulmer@tum.de>
2020-06-17 22:00:47 +0200

    minor changes to test script

--- real_robot_dEE_impedance.py
+++ real_robot_dEE_impedance.py
@@ -4,6 +4,7 @@ from sai2_environment.action_space import ActionSpace
 
 import numpy as np
 import time
+from PIL import Image
 
 
 def main():
@@ -12,7 +13,7 @@ def main():
     blocking_action = True
 
     env = RobotEnv(name='move_object_to_target',
-                   simulation=False,
+                   simulation=True,
                    action_space=action_space,
                    isotropic_gains=True,
                    render=True,
@@ -20,16 +21,16 @@ def main():
                    rotation_axis=(0, 0, 1))    
 
     episodes = 50
-    steps = 300
+    steps = 200
 
-    start_time = time.time()
+    start_time = time.time()    
 
     for episode in range(episodes):
         
         print("Episode: {}; Elapsed Time: {} minutes".format(episode, round((time.time()-start_time)/60), 4))
         obs = env.reset()
         for step in range(steps):
-            position = np.around(np.random.uniform(low=-0.15, high=0.15, size=(3,)), 2)
+            position = np.random.uniform(low=-0.1, high=0.1, size=(3,))
             rotation = np.around(np.random.uniform(low=-0.1, high=0.1, size=(1,)), 2)
             stiffness_linear = np.around(np.random.uniform(low=-50, high=50, size=(1,)),2)
             stiffness_rot = np.around(np.random.uniform(low=-2, high=2, size=(1,)), 2)
@@ -39,12 +40,6 @@ def main():
             if not blocking_action:
                 time.sleep(0.1)
             obs, reward, done, info = env.step(action)
-<<<<<<< HEAD
-=======
-            #time.sleep(1.0)
->>>>>>> fcb19445e9d230ffae0fd3d4e6b5165b239ea330
-
-
 
 if __name__ == "__main__":
     main()
