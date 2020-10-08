c594e06d424fd957dc14ba6d1ddd1bc3b61f30bc
Jerrycj <jerrycj@gmail.com>
2020-10-08 20:30:15 +0200

    fix conflicts and delete temp files

--- real_robot_dEE_impedance.py
+++ real_robot_dEE_impedance.py
@@ -14,18 +14,6 @@ def main():
 
     action_space = ActionSpace.MT_EE_POSE_IMPEDANCE
 
-<<<<<<< HEAD
-    env = RobotEnv(name='move_object_to_target',
-                   simulation=False,
-                   action_space=action_space,
-                   action_frequency=20,
-                   render=True,
-                   camera_available=True,
-                   rotation_axis=(0, 0, 0))    
-
-    episodes = 1
-    steps = 500
-=======
     env = RobotEnv(
         domain_name="reach_site",
         task_name="easy",
@@ -40,43 +28,23 @@ def main():
 
     episodes = 10
     steps = 200
->>>>>>> master
 
     start_time = time.time()
 
     for episode in range(episodes):
-<<<<<<< HEAD
-        
-        print("Episode: {}; Elapsed Time: {} minutes".format(episode, round((time.time()-start_time)/60), 4))
-        
-=======
 
         print(
             "Episode: {}; Elapsed Time: {} minutes".format(
                 episode, round((time.time() - start_time) / 60), 4
             )
         )
->>>>>>> master
         obs = env.reset()
-        
-        
+
         acc_reward = 0
         for step in range(steps):
             position = np.random.normal(loc=0.0, scale=0.1, size=(3,))
             stiffness_linear = np.random.normal(loc=0.0, scale=10, size=(1,))
             stiffness_rot = np.random.normal(loc=0.0, scale=1, size=(1,))
-<<<<<<< HEAD
-            action = np.concatenate((position,stiffness_linear,stiffness_rot))
-            # action = env.act_optimally()
-            # print(action)
-
-            # obs, reward, done, info = env.step(action)
-            # obs = env.reset()
-            # if done:                
-            #     obs = env.reset()
-            #     continue
-                
-=======
             action = np.concatenate(
                 (position, stiffness_linear, stiffness_rot)
             )
@@ -91,7 +59,6 @@ def main():
                 # obs = env.reset()
                 continue
 
->>>>>>> master
             # if render:
             #     im = obs[0]
             #     if im is not None:
@@ -101,10 +68,6 @@ def main():
             #     if key & 0xFF == ord('q') or key == 27:
             #         cv2.destroyAllWindows()
             #         break
-<<<<<<< HEAD
-        # env.close()
-=======
->>>>>>> master
 
     print(
         "Action frequency: {}".format(
